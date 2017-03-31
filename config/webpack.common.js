2b3860524ce4a702a6dd2f1f55e6d75814d21ed0
Blinnikov <igor.blinnikov@gmail.com>
2017-03-31 15:29:54 +0300

    feat(init): webpack configs

---
+++ config/webpack.common.js
@@ -0,0 +1,32 @@
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: { configFileName: helpers.root('src', 'tsconfig.json') }
+          },
+          'angular2-template-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  }
+
+}
