e4e738a20395f052706f524058159128216f9fa0
Blinnikov <igor.blinnikov@gmail.com>
2017-04-05 17:42:36 +0300

    feat(init): angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
+import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import 'hammerjs';
+import '@angular/material/core/theming/prebuilt/indigo-pink.css'
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, MaterialModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
