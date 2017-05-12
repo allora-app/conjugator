14f245c77cea849f85502abc4289e6708116dcfb
Blinnikov <igor.blinnikov@gmail.com>
2017-05-12 17:41:24 +0300

    chore(layout): flex layout stub

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'search-results',
-  templateUrl: './search-results.component.html'
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent { }
